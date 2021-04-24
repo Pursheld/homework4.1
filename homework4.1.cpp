f5de790fcfac7297ac0cb4c9da6da229ccaf48bc
Pursheld <stompee@bk.ru>
2021-04-24 15:08:28 +0300

    Добавьте файлы проекта.

---
+++ homework4.1.cpp
@@ -0,0 +1,26 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+	int sum = 0;
+	int index;
+	const int N = 3;
+	int arr[N][N] = { {1,2,3} , {1,2,3} };
+
+	time_t now = time(0);
+	tm* ltm = localtime(&now);
+	int day = ltm->tm_mday;
+
+	for(int i=0;i<N;i++)
+	{
+		
+			index = day / N;
+			sum += arr[i][index];
+			
+			
+	}
+	cout << sum;
+}
